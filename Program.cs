cf3ad675a3f0b8d5211cbed39a7721211d443583
emmac <emmac@LAPTOP-B7LJ287N>
2021-11-11 22:52:52 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Exercice 9 : saisie d'une note
+ * Author : Emma Caillé
+ * Date : 09/11/2021
+ */
+using System;
+
+namespace Exercice9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // déclaration
+            int note;
+
+            // saisie de la note avec contrôle
+            do
+            {
+                Console.Write("Entrez une note (entre 0 et 20) = ");
+                note = int.Parse(Console.ReadLine());
+            } while (note<0 || note>20);
+           
+
+            // affichage de la note correcte
+            Console.WriteLine("note saisie = " + note);
+            Console.ReadLine();
+        }
+    }
+}
